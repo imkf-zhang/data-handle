a1d620af491abfa726a7cd7ad925831ed0186f66
imkf-zhang <15638452918@163.com>
2022-02-11 22:50:26 +0800

    [study]study

---
+++ 循环/8、js中的几种循环.js
@@ -0,0 +1,13 @@
+// for语句也是一种前测试循环语句 只有当条件表达式（index<count）返回true的情况下才会进入for循环
+let count = 10;
+for (let index = 0; index < count; index++) {
+  console.log(index)
+}
+
+// 用while语句转变上述的for循环
+let countt = 10;
+let i = 0;
+while (i < countt) {
+  console.log(i)
+  i++
+}
