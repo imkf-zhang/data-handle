30a1faea9d5b4c45da7027c803e5a73886e29770
imkf-zhang <15638452918@163.com>
2022-03-19 00:04:28 +0800

    feat：study

--- math/1、Math_abs.js
+++ math/1、Math_abs.js
@@ -1,3 +1,4 @@
+// Math.abs(x) 函数返回指定数字 “x“ 的绝对值。如下：
 let a = -888
 console.log(Math.abs(a)) // 888
 
--- math/7、Math_sqrt.js
+++ math/7、Math_sqrt.js
@@ -1,2 +1,3 @@
+// Math.sqrt() 函数返回一个数的平方根
 console.log(Math.sqrt(9)) // 3
 console.log(Math.sqrt(4)) // 2
